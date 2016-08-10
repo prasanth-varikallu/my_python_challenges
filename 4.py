e5d53da9b5d7edd6f36b7103fefb5c7b19167a1b
Prasanth V <prasanth-varikallu@users.noreply.github.com>
2016-08-09 23:26:42 -0400

    a small guy between three Big guys not four exactly 3

--- 4.py
+++ 4.py
@@ -1251,4 +1251,4 @@ dCdFLtBQPtFQuCdKOrpndJNUFQIDSbetUKylhSUjcDVtbiQrWMRQhAwGUZyPneCGUjGBBTkLqxLAXXtB
 KfErkDaWMFZZeuqDmXKJEGHyToPUhPphfVhgUZgbIuRAtWnroImpJKqqmEZqeNQCKzhjIkKQHURWLXFw
 PBuijeoTSpsVLaOGuLVjMZXkBvVXwUuHfBihziiavGSYofPNeKsTXruMUumRRPQJzvSzJkKbtSipiqBd'''
 
-print ''.join(re.findall(r'[A-Z]{3}([a-z])[A-Z]{3}', text))
+print ''.join(re.findall(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]', text))
