caf0956fd123eab1ddd39c806eff9df3b5ab51d4
Prasanth V <prasanth-varikallu@users.noreply.github.com>
2016-08-10 21:15:21 -0400

    Found the peak

--- 5.py
+++ 5.py
@@ -3,12 +3,11 @@ import urllib, re
 base_url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
 base_number = '12345'
 i = 0
-while (i < 100):
+while (i < 400):
 	resp = urllib.urlopen(base_url + base_number)
 	text = resp.read()
+	if not (re.search(ur'and the next nothing is [0-9]+', text)):
+		print text
 	base_number = ''.join(re.findall(r'[0-9]', text))
 	i += 1
-	print i, base_number
-
-print base_number, i
 
